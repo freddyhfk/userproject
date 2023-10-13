15664ddc0b3e8a398b5ebf2ed87ea1cb69975e9c
freddyhfk <freddyhfk@t-online.de>
2023-10-14 00:55:48 +0200

    initial commit

---
+++ src/test/java/com/example/userproject/UserProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.userproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
