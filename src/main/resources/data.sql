a70b3667edea9cc22fc9dc3153a1440826d6c42f
freddyhfk <freddyhfk@t-online.de>
2023-10-14 19:03:44 +0200

    created data and schema scripts for some test data

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO test (name, surname, email) VALUES ('Tom', 'Eckig', 'corner@gmail.com');
+INSERT INTO test (name, surname, email) VALUES ('Simon', 'Stumpf', 'blunt@gmail.com');
+INSERT INTO test (name, surname, email) VALUES ('Peter', 'Kantig', 'angular@gmail.com');
+INSERT INTO test (name, surname, email) VALUES ('Tom', 'Rund', 'round@gmail.com');
+INSERT INTO test (name, surname, email) VALUES ('Anton', 'Krumm', 'crooked@gmail.com');
---
+++ src/main/resources/schema.sql
@@ -0,0 +1,9 @@
+drop table if exists test;
+create table test(
+    id bigint auto_increment,
+    name varchar(255),
+    surname varchar(255),
+    email varchar(255)
+);
+
+
